2f0636bdc4abd80b845f3db91ed46f6a505d1c05
naeemqaswar <naeemqaswar@gmail.com>
2021-05-21 00:14:06 +0500

    Updated portfolio filters, images, description and links

--- js/main.js
+++ js/main.js
@@ -1,111 +1,136 @@
 /**
-* Template Name: MyPortfolio - v4.1.0
-* Template URL: https://bootstrapmade.com/myportfolio-bootstrap-portfolio-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+ * Template Name: MyPortfolio - v4.1.0
+ * Template URL: https://bootstrapmade.com/myportfolio-bootstrap-portfolio-website-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function () {
   "use strict";
 
   /**
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el.trim();
     if (all) {
-      return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)];
     } else {
-      return document.querySelector(el)
+      return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = select(el, all);
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach((e) => e.addEventListener(type, listener));
       } else {
-        selectEl.addEventListener(type, listener)
+        selectEl.addEventListener(type, listener);
       }
     }
-  }
+  };
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+    el.addEventListener("scroll", listener);
+  };
 
   /**
    * burgerMenu
    */
-  const burgerMenu = select('.burger')
-  on('click', '.burger', function(e) {
-    burgerMenu.classList.toggle('active');
-  })
+  const burgerMenu = select(".burger");
+  on("click", ".burger", function (e) {
+    burgerMenu.classList.toggle("active");
+  });
 
   /**
    * Porfolio isotope and filter
    */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('#portfolio-grid');
+  var portfolioIsotope;
+  var portfolioFilters;
+  window.addEventListener("load", () => {
+    let _directFilterRequest = getParameterByName("tech");
+    let portfolioContainer = select("#portfolio-grid");
+
     if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.item',
+      portfolioFilters = select("#filters a", true);
+      portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: ".item",
       });
 
-      let portfolioFilters = select('#filters a', true);
+      if (_directFilterRequest) {
+        filterPortfolioItems(`.${_directFilterRequest}`);
+      }
 
-      on('click', '#filters a', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('active');
-        });
-        this.classList.add('active');
+      on(
+        "click",
+        "#filters a",
+        function (e) {
+          e.preventDefault();
 
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
+          filterPortfolioItems(this.getAttribute("data-filter"));
+        },
+        true
+      );
     }
-
   });
+
+  function filterPortfolioItems(filter) {
+    portfolioFilters.forEach(function (el) {
+      el.classList.remove("active");
+    });
+
+    select(`#filters a[data-filter="${filter}"]`).classList.add("active");
+
+    portfolioIsotope.arrange({ filter });
+    portfolioIsotope.on("arrangeComplete", function () {
+      AOS.refresh();
+    });
+  }
+
+  function getParameterByName(name, url = window.location.href) {
+    if (!name) return;
+
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return "";
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 
   /**
    * Testimonials slider
    */
-  new Swiper('.testimonials-slider', {
+  new Swiper(".testimonials-slider", {
     speed: 600,
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
   });
 
   /**
    * Animation on scroll
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
       once: true,
-      mirror: false
-    })
+      mirror: false,
+    });
   });
-
-})()
+})();
